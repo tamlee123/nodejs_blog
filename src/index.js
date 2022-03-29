747b161209051a204f171be166f694395de616c0
tam <cherykim123@gmail.com>
2022-03-28 17:03:21 -0700

    directory structure with express-handlebars

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import morgan from "morgan";
+import { engine } from "express-handlebars";
+import path from "path";
+const app = express();
+const port = 3000;
+
+//HTTP logger
+app.use(morgan("combined"));
+
+//Template engine
+app.engine("hbs", engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+const __dirname = path.resolve(); // need to define this for using es6 module
+app.set("views", path.join(__dirname, "src/resource/views"));
+
+app.get("/", (req, res) => res.render("home"));
+app.get("/news", (req, res) => res.render("news"));
+
+//127.0.0.1 - localhost
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
