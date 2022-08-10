937903bb85f9f332f5da6bb014c70518d452f9e3
tam <cherykim123@gmail.com>
2022-08-10 16:48:04 -0700

    run husky

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import express from 'express';
-import morgan from 'morgan';
-import { engine } from 'express-handlebars';
-import path from 'path';
-import route from './routes/index.js';
+import express from "express";
+import morgan from "morgan";
+import { engine } from "express-handlebars";
+import path from "path";
+import route from "./routes/index.js";
 
 const app = express();
 const port = 3000;
 
 const __dirname = path.resolve(); // need to define this for using es6 module
-app.use(express.static(path.join(__dirname, 'src/public')));
+app.use(express.static(path.join(__dirname, "src/public")));
 
 //Middleware
 app.use(express.urlencoded({ extended: true })); //form html
@@ -18,14 +18,14 @@ app.use(express.json()); // code javascript: XMLHttpResquest, fetch, axios,...
 // app.use(morgan("combined"));
 
 //Template engine
-app.engine('hbs', engine({ extname: '.hbs' }));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'src/resource/views'));
+app.engine("hbs", engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "src/resource/views"));
 
 //Routes init
 route(app);
 
 //127.0.0.1 - localhost
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
